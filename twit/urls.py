5388cf2ab9003283572cb855eee83ecdd65195c2
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-06-09 00:14:17 +0100

    setting up the API

--- twit/urls.py
+++ twit/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home)
+    path("", views.home, name = "home"),
+    path("status/", views.status, name = "status")
 ]
--- twit/views.py
+++ twit/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from twilio.rest import Client
 
 # Create your views here.
 
 def home(request):
+
+
     return render(request, 'twit/home.html')
+
+def status(request):
+    phone = request.GET.get('phone')
+    message = request.GET.get('message')
+    account_sid = 'AC084b5641a2435e72c3e1e894cf57db18'
+    auth_token = '5900504d34f0a41ce842b2ab9c91af5a'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+            body=f'{message}',
+            from_='+12013791613',
+            to=f"{phone}"
+        )
+
+    print(message.sid)
+    return render(request, 'twit/status.html')
